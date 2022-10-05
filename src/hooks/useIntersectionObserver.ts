d9cb53693e04c52ac8e6195a9d9a9741353c011f
JengYoung <young_pallete@naver.com>
2022-10-05 13:38:53 +0900

    :sparkles: useIntersectionObserver 구현, 테스트 미적용
    느낌상으로 다음을 참고하며 작성해보았다.
    https://developer.mozilla.org/ko/docs/Web/API/Intersection_Observer_API

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,9 +1,32 @@
+import { RefObject, useEffect, useRef } from 'react';
+
+type CallbackType = IntersectionObserverCallback;
 const useIntersectionObserver = (
-  targetRef: HTMLElement,
+  targetRef: RefObject<Element>,
+  callback: CallbackType,
   { root, rootMargin, threshold }: IntersectionObserverInit
 ) => {
-  const option = { root, rootMargin, threshold };
-  return [targetRef, option];
+  const options = { root, rootMargin, threshold };
+  const callbackRef = useRef<CallbackType | null>(null);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callbackRef]);
+
+  useEffect(() => {
+    if (callbackRef.current === null || targetRef.current === null) return;
+
+    const target = targetRef.current;
+    const observer = new IntersectionObserver(callbackRef.current, options);
+
+    observer.observe(target);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [targetRef, callbackRef]);
+
+  return [targetRef, callbackRef];
 };
 
 export default useIntersectionObserver;
