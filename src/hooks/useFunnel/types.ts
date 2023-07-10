53eaebb7669af923b320d6b33550d735ef9ffbbc
JengYoung <young_pallete@naver.com>
2023-07-10 23:47:56 +0900

    :sparkles: Create funnel example

---
+++ src/hooks/useFunnel/types.ts
@@ -0,0 +1,6 @@
+export type TFunnelNavigator = {
+  prev?: string;
+  next?: string;
+  onPrev?: () => void;
+  onNext?: () => void;
+}
--- src/pages/Funnel/index.tsx
+++ src/pages/Funnel/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useFunnel } from '../../hooks/useFunnel/useFunnel'
 
 export function FunnelPage() {
+  const {step, Funnel, go} = useFunnel({ entry: 'hello' });
+
   return (
-    <div>index</div>
+    <Funnel step={step}>
+      <Funnel.Step name="hello">
+        Hello!
+        <button type="button" onClick={() => go('hello2')}>CLICK!</button>
+      </Funnel.Step>
+
+      <Funnel.Step name="hello2">
+        Hello!!!
+        <button type="button" onClick={() => go('hello')}>CLICK!</button>
+      </Funnel.Step>
+
+      <div>hi2</div>
+    </Funnel>
   )
 }
