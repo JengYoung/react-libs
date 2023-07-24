d7c6ad2e975db3712eabd0694506e7fb69ba205f
JengYoung <young_pallete@naver.com>
2023-07-24 23:59:37 +0900

    :sparkles: Create ValidFallback, isValid

--- src/hooks/useFunnel/Step.tsx
+++ src/hooks/useFunnel/Step.tsx
@@ -1,9 +1,9 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, ReactNode } from "react";
 
-export type TStepProps =  { name: string } & PropsWithChildren;
+export type TStepProps =  { name: string; isValid?: boolean; validFallback?: ReactNode } & PropsWithChildren;
 
-export function Step({ name, children }: TStepProps) {
-  if (!name) return <div />;
+export function Step({ name, children, isValid = true, validFallback = <div /> }: TStepProps) {
+  if (!name || !isValid) return validFallback;
 
   return (
     children
