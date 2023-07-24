40df004d3befd33e76c2d0c611fd598467683dc8
JengYoung <young_pallete@naver.com>
2023-07-24 23:43:57 +0900

    :sparkles: Create pop in useFunnel

--- src/hooks/useFunnel/useFunnel.ts
+++ src/hooks/useFunnel/useFunnel.ts
@@ -7,19 +7,40 @@ export type TUseFunnelParams = {
 
 export type TFunnelState = {
   step: string;
+  histories: string[];
 }
 
 export const useFunnel = ({ entry }: TUseFunnelParams) => {
-  const [funnelState, setFunnelState] = useState<TFunnelState>({ step: entry });
+  const [funnelState, setFunnelState] = useState<TFunnelState>({ step: entry, histories: [] });
 
 
   const go = (key: string) => {
-    setFunnelState(state => ({ ...state, step: key }));
+    setFunnelState(state => ({ ...state, step: key, histories: [...state.histories, state.step] }));
   }
   
+  const isEntry = !funnelState.histories.length
+
+  const pop = () => {
+    if (isEntry) return;
+
+    setFunnelState(state => {
+      const nextHistories = [...state.histories];
+      const nextStep = nextHistories.pop() ?? state.step;
+
+      return {
+        ...state,
+        step: nextStep,
+        histories: nextHistories,
+      }
+    })
+  }
+
+
   return {
-    go,
     Funnel,
     step: funnelState.step,
+    isEntry,
+    go,
+    pop,
   }
 }
--- src/pages/Funnel/index.tsx
+++ src/pages/Funnel/index.tsx
@@ -2,18 +2,26 @@ import React from 'react'
 import { useFunnel } from '../../hooks/useFunnel/useFunnel'
 
 export function FunnelPage() {
-  const {step, Funnel, go} = useFunnel({ entry: 'hello' });
+  const {step, Funnel, go, pop} = useFunnel({ entry: 'hello' });
 
   return (
     <Funnel step={step}>
       <Funnel.Step name="hello">
-        Hello!
-        <button type="button" onClick={() => go('hello2')}>CLICK!</button>
+        Hello!1
+        <button type="button" onClick={() => go('hello3')}>CLICK!</button>
+        <button type="button" onClick={() => pop()}>POP!</button>
       </Funnel.Step>
 
       <Funnel.Step name="hello2">
-        Hello!!!
+        Hello!!!2
         <button type="button" onClick={() => go('hello')}>CLICK!</button>
+        <button type="button" onClick={() => pop()}>POP!</button>
+      </Funnel.Step>
+
+      <Funnel.Step name="hello3">
+        Hello!!!3
+        <button type="button" onClick={() => go('hello2')}>CLICK!</button>
+        <button type="button" onClick={() => pop()}>POP!</button>
       </Funnel.Step>
 
       <div>hi2</div>
